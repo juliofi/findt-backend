b3550faf6f16b9391ae8ef94d2500f8c7f611d05
juliopontocom <146362643+juliopontocom@users.noreply.github.com>
2025-06-04 11:54:27 -0300

    projeto spring sem banco ainda

---
+++ src/main/java/com/julio/backfindt/BackfindtApplication.java
@@ -0,0 +1,13 @@
+package com.julio.backfindt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackfindtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackfindtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/julio/backfindt/BackfindtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julio.backfindt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackfindtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

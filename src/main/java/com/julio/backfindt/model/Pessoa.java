47f99250de59dd795ae713152e8f8ab240be671e
juliopontocom <146362643+juliopontocom@users.noreply.github.com>
2025-06-05 15:48:13 -0300

    modelo pessoa

---
+++ src/main/java/com/julio/backfindt/model/Pessoa.java
@@ -0,0 +1,26 @@
+package com.julio.backfindt.model;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+@Entity
+@Data
+@NoArgsConstructor
+@Builder
+public class Pessoa {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Login id;
+
+    @Column(name = "nome_completo", nullable = false)
+    private String nomeCompleto;
+
+    @Column(nullable = false, unique = true)
+    private String cpf;
+
+    @Column(nullable = false)
+    private String genero;
+
+    @Column(name = "data_nascimento", nullable = false)
+    private LocalDate dataNascimento;
+}
